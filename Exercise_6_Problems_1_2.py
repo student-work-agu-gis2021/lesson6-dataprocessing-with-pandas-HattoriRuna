a1ac9c9291eab035fbb78fda22a87993bea95d69
HattoriRuna <>
2021-05-23 07:25:59 +0000

    I added a line to find the latest observation data. (YOUR CODE HERE 5)

--- Exercise_6_Problems_1_2.py
+++ Exercise_6_Problems_1_2.py
@@ -88,6 +88,8 @@ print("Number of days:", day_count)
 first_obs = None
  
 # YOUR CODE HERE 5
+# Find the date of the oldest (first) observation
+first_obs = min(data['DATE'])
 
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
